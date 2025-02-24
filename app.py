a317c407f77040af8f8b56c1d13bc26d42a3f5a4
Orkhan <baku.vip01@gmail.com>
2025-02-24 14:08:31 +0400

    health

--- app.py
+++ app.py
@@ -53,6 +53,10 @@ app.add_middleware(
 for route in routers:
     app.include_router(route)
 
+@app.get("/health")
+async def health_check():
+    return {"status": "ok", "message": "Service is up and running"}
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)  # Ensure port matches Dockerfile
